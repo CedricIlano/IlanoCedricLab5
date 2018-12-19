414cbdb18b739339f0e33d8243aa1757c7cb2795
CedricIlano <2015081167@ust-ics.mygbiz.com>
2018-12-19 21:25:17 +0800

    .

--- app/src/main/java/com/ilano/cedric/activity2.java
+++ app/src/main/java/com/ilano/cedric/activity2.java
@@ -1,20 +1,21 @@
 package com.ilano.cedric;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class activity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity2);
     }
 
-    public void activity2 (View v) {
-        Intent intent = new Intent(this,activity2.class);
+    public void activity1 (View v) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
+
